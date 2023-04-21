5f3bb36260e7b26f6968a6e8df24c995ea5b80c5
meshdako <msobarzo@utem.cl>
2023-04-21 13:03:44 -0400

    Agregado los métodos de GET, PUT y DELETE de Facturas, Proveedor y Registro_Solicitud (3)

--- src/Proyecto_API/api/views.py
+++ src/Proyecto_API/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
@@ -6,6 +7,14 @@ from .models import *
 import json
 
 # Create your views here.
+
+def Facturas(request):
+    facturas = Factura.objects.all()
+    return render(request = request, template_name="main/facturas.html", context={'facturas':facturas})
+
+def Products(request):
+    productos = Producto.objects.all()
+    return render(request = request, template_name="main/products.html", context={'productos':productos})
 
 class ProductView(View):
 
@@ -20,7 +29,7 @@ class ProductView(View):
                 producto = productos[0]
                 datos={'message':"Success", 'producto':producto}
             else:
-                datos={'message':"Producto no encontrados..."}
+                datos={'message':"Producto no encontrado..."}
             return JsonResponse(datos)
         else:
             productos=list(Producto.objects.values())
@@ -61,4 +70,154 @@ class ProductView(View):
             datos = {'message': 'Success'}
         else:
             datos = {'message': "Producto no encontrado..."}
+        return JsonResponse(datos)
+
+class FacturaView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if (id > 0):
+            facturas = list(Factura.objects.filter(id=id).values())
+            if len(facturas) > 0:
+                factura = facturas[0]
+                datos={'message':"Success", 'factura':factura}
+            else:
+                datos={'message':"Factura no encontrada..."}
+            return JsonResponse(datos)
+        else:
+            facturas=list(Factura.objects.values())
+            if len(facturas) > 0:
+                datos={'message':'Success', 'facturas':facturas}
+            else:
+                datos={'message':"Facturas no encontradas..."}
+            return JsonResponse(datos)
+
+    def post(self, request):
+        pass
+
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        factura = list(Factura.objects.filter(id=id).values())
+        if len(factura) > 0:
+            factura = Factura.objects.get(id=id)
+            factura.fecha_compra=jd['fecha_compra']
+            factura.valor_neto=jd['valor_neto']
+            factura.iva=jd['iva']
+            factura.total=jd['total']
+            factura.id_proveedor_id=jd['id_proveedor_id']
+            factura.save()
+            datos = {'message': 'Success'}
+        else:
+            datos={'message':"Factura no encontrada..."}
+        return JsonResponse(datos)
+
+    def delete(self, request, id):
+        factura = list(Factura.objects.filter(id=id).values())
+        if len(factura) > 0:
+            Factura.objects.filter(id=id).delete(),
+            datos = {'message': 'Success'}
+        else:
+            datos = {'message': "Factura no encontrada..."}
+        return JsonResponse(datos)
+
+class ProveedorView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if (id > 0):
+            proveedores = list(Proveedor.objects.filter(id=id).values())
+            if len(proveedores) > 0:
+                proveedor = proveedores[0]
+                datos={'message':"Success", 'proveedor':proveedor}
+            else:
+                datos={'message':"Proveedor no encontrado..."}
+            return JsonResponse(datos)
+        else:
+            proveedores=list(Proveedor.objects.values())
+            if len(proveedores) > 0:
+                datos={'message':'Success', 'proveedores':proveedores}
+            else:
+                datos={'message':"Proveedores no encontrados..."}
+            return JsonResponse(datos)
+
+    def post(self, request):
+        pass
+
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        proveedor = list(Proveedor.objects.filter(id=id).values())
+        if len(proveedor) > 0:
+            proveedor = Proveedor.objects.get(id=id)
+            proveedor.rut=jd['rut']
+            proveedor.nombre=jd['nombre']
+            proveedor.giro=jd['giro']
+            proveedor.direcc=jd['direcc']
+            proveedor.telefono=jd['telefono']
+            proveedor.save()
+            datos = {'message': 'Success'}
+        else:
+            datos={'message':"Proveedor no encontrado..."}
+        return JsonResponse(datos)
+
+    def delete(self, request, id):
+        proveedor = list(Proveedor.objects.filter(id=id).values())
+        if len(proveedor) > 0:
+            Proveedor.objects.filter(id=id).delete(),
+            datos = {'message': 'Success'}
+        else:
+            datos = {'message': "Proveedor no encontrado..."}
+        return JsonResponse(datos)
+    
+class RegistroView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if (id > 0):
+            registros = list(Registro_Solicitud.objects.filter(id=id).values())
+            if len(registros) > 0:
+                registro = registros[0]
+                datos={'message':"Success", 'registro':registro}
+            else:
+                datos={'message':"Registro no encontrado..."}
+            return JsonResponse(datos)
+        else:
+            registros=list(Registro_Solicitud.objects.values())
+            if len(registros) > 0:
+                datos={'message':'Success', 'registros':registros}
+            else:
+                datos={'message':"Registros no encontrados..."}
+            return JsonResponse(datos)
+
+    def post(self, request):
+        pass
+
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        registro = list(Registro_Solicitud.objects.filter(id=id).values())
+        if len(registro) > 0:
+            registro = Registro_Solicitud.objects.get(id=id)
+            registro.rut=jd['rut']
+            registro.nombre=jd['nombre']
+            registro.save()
+            datos = {'message': 'Success'}
+        else:
+            datos={'message':"Registro no encontrado..."}
+        return JsonResponse(datos)
+
+    def delete(self, request, id):
+        registro = list(Registro_Solicitud.objects.filter(id=id).values())
+        if len(registro) > 0:
+            Registro_Solicitud.objects.filter(id=id).delete(),
+            datos = {'message': 'Success'}
+        else:
+            datos = {'message': "Registro no encontrado..."}
         return JsonResponse(datos)
