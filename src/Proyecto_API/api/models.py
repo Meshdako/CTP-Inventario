d63e71c06362712e69c6cd2b392353c4031746ba
meshdako <msobarzo@utem.cl>
2023-05-05 15:59:01 -0400

    Modelos actualizados en base a la BD

--- src/Proyecto_API/api/models.py
+++ src/Proyecto_API/api/models.py
@@ -11,22 +11,35 @@ class Proveedor(models.Model):
     rut = models.PositiveIntegerField()
     nombre = models.CharField(max_length=50)
     giro = models.CharField(max_length=100)
-    direcc = models.ForeignKey(Direccion, on_delete=models.CASCADE)
+    correo = models.EmailField(max_length=254)
     telefono = models.PositiveIntegerField()
+    celular = models.PositiveIntegerField()
+    web = models.URLField(max_length=200)
+    ubicacion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
 
 class Factura(models.Model):
     fecha_compra = models.DateField()
     valor_neto = models.PositiveIntegerField()
     iva = models.PositiveIntegerField()
     total = models.PositiveIntegerField()
-    id_proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
 
-class Producto(models.Model):
-    prod_nombre = models.CharField(max_length=50)
-    cant = models.PositiveIntegerField()
-    precio_unit = models.PositiveIntegerField()
+class Categoria(models.Model):
+    TIPO_CATEGORIA = [
+        (1, "Mobiliario"),
+        (2, "Aseo"),
+        (3, "Quimica")
+    ]
+    tipo_categoria = models.PositiveIntegerField(choices=TIPO_CATEGORIA)
+
+class Articulo(models.Model):
+    codigo = models.PositiveIntegerField()
+    nombre_articulo = models.CharField(max_length=50)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.PositiveIntegerField()
     total = models.PositiveIntegerField()
     factura_detalle = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
 class Registro_Solicitud(models.Model):
     fecha = models.DateTimeField()
