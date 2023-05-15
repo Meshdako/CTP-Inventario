c7b97f0ee8fd2e560fdc710b290dcec7d632bf20
meshdako <msobarzo@utem.cl>
2023-05-15 16:31:55 -0400

    Actualizada las URLs para la plantilla creada, añadir factura

--- src/Proyecto_API/api/urls.py
+++ src/Proyecto_API/api/urls.py
@@ -18,7 +18,8 @@ urlpatterns=[
     path('registros/<int:id>', RegistroView.as_view(), name='registro_process'),
     # URLs Plantillas
     path("home/views", Home, name="home"),
-    path("articulos/views", Products, name="productos"),
+    path("articulos/views", Products, name="articulos"),
     path("articulos/add_item", Add_Item, name="facturas"),
-    path("facturas/views", Facturas, name="facturas")
+    path("facturas/views", Facturas, name="facturas"),
+    path("facturas/add_factura", Add_Factura)
 ]
